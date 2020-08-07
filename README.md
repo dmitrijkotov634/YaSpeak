e02499bbb3e48f7b65525d915aa9b6f04ff74fa9
dmitrijkotov634 <dmitrijkotov634@gmail.com>
2020-08-07 22:16:05 +0300

    Start

---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+	<h1>YaSpeak</h1>
+
+Python wrapper for Yandex text voiceover
+
+![pypi](https://badge.fury.io/py/YaSpeak.svg)
+
+</div>
+
+## Install
+`pip install --upgrade yaspeak`
+
+## Usage
+```python
+from yaspeak import YaSpeak
+
+ysp = YaSpeak()
+
+ysp.oksana("hello", "oksana.mp3")
+ysp.levitan("hello", "levitan.mp3")
+ysp.yandex("hello", "yandex.mp3")
+```
---
+++ yaspeak/yaspeak.py
@@ -0,0 +1,15 @@
+﻿import requests
+
+class YaSpeak:
+    def __init__(self):
+	    pass
+
+    def __getattr__(self, speaker):
+        return lambda *data: self.say(speaker, *data)
+        
+    def say(self, speaker, text, file="out.mp3"):
+        data = {"text": text}
+        if speaker != "yandex": data.update({"speaker": speaker})
+        output = requests.get("https://station.aimylogic.com/generate", data=data)
+        with open(file, 'wb') as f:
+            f.write(output.content)
